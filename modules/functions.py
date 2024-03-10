a600061176408c26c8426da0e2f2ca03346a5a24
fmathews11 <fmathews@gmail.com>
2024-03-10 09:25:24 -0400

    Added 2 new functions

--- modules/functions.py
+++ modules/functions.py
@@ -3,10 +3,18 @@ import requests
 from global_variables import CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN
 
 token_auth_url = 'https://www.strava.com/oauth/token'
+activities_endpoint = "https://www.strava.com/api/v3/athlete/activities"
+
+
+# Internal use
+def _validate_status_code(response_object: requests.models.Response):
+    """Quick function to validate status code"""
+    if response_object.status_code != 200:
+        print(response_object.text)
+        raise ValueError(f"Response status code was {response_object.status_code}")
 
 
 def generate_access_token() -> str:
-
     """Uses the client ID, secret, and authorization-specific refresh token to generate a temporary
     access token"""
 
@@ -18,9 +26,18 @@ def generate_access_token() -> str:
         'f': 'json'}
 
     response = requests.post(url=token_auth_url, params=param_dict)
-
-    if response.status_code != 200:
-        print(response.text)
-        raise ValueError(f"Response status code was {response.status_code}")
-
+    _validate_status_code(response_object=response)
     return response.json()['access_token']
+
+
+def get_activity_data(access_token: str,params: dict = None) -> dict:
+    """
+    Returns a dictionary (response.json()) representing the user's 200 most recent activities
+    """
+    headers = {'Authorization': f'Authorization: Bearer {access_token}'}
+    if not params:
+        response = requests.get(activities_endpoint, headers=headers)
+    else:
+        response = requests.get(activities_endpoint, headers=headers, params=params)
+    _validate_status_code(response_object=response)
+    return response.json()
