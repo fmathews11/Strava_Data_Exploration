59bbf5d49d0deca5112b7de5f83dbbbd448e0ce3
fmathews11 <frank.mathews@handleglobal.com>
2024-07-08 11:33:03 -0400

    updated function to reflect changes in API functionality

--- modules/functions.py
+++ modules/functions.py
@@ -1,9 +1,14 @@
 import requests
-
 from global_variables import CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN
 
 token_auth_url = 'https://www.strava.com/oauth/token'
 activities_endpoint = "https://www.strava.com/api/v3/athlete/activities"
+
+token_param_dict = {'client_id': CLIENT_ID,
+                    'client_secret': CLIENT_SECRET,
+                    'refresh_token': REFRESH_TOKEN,
+                    'grant_type': "refresh_token",
+                    'f': 'json'}
 
 
 # Internal use
@@ -18,21 +23,14 @@ def generate_access_token() -> str:
     """Uses the client ID, secret, and authorization-specific refresh token to generate a temporary
     access token"""
 
-    param_dict = {
-        'client_id': CLIENT_ID,
-        'client_secret': CLIENT_SECRET,
-        'refresh_token': REFRESH_TOKEN,
-        'grant_type': "refresh_token",
-        'f': 'json'}
-
-    response = requests.post(url=token_auth_url, params=param_dict)
+    response = requests.post(url=token_auth_url, params=token_param_dict)
     _validate_status_code(response_object=response)
     return response.json()['access_token']
 
 
-def get_activity_data(access_token: str,params: dict = None) -> dict:
+def get_activity_data(access_token: str, params: dict = None) -> dict:
     """
-    Returns a dictionary (response.json()) representing the user's 200 most recent activities
+    Returns a dictionary (response.json()) representing the user's 30 most recent activities
     """
     headers = {'Authorization': f'Authorization: Bearer {access_token}'}
     if not params:
