0265ef22c37597d7b29bdabd2ba6b6ded1eeb710
fmathews11 <frank.mathews@handleglobal.com>
2024-07-12 15:49:58 -0400

    Further streamlined functionality of RideHub

--- modules/objects.py
+++ modules/objects.py
@@ -23,12 +23,20 @@ class StravaRide:
 
 
 class RideHub:
-    """Class to house StravaRide objects"""
+    """
+    Class to house StravaRide objects.
+    The data will be pulled in and written out as JSON files, but this object was created to more easily interact
+    with the data
+    """
     _ride_list = []
 
     def __init__(self, *args):
-        if args:
-            self._ride_list.extend(args)
+
+        for sub_dict in args:
+            temp_ride_object = StravaRide(sub_dict['id'],
+                                          metadata=sub_dict['metadata'],
+                                          metrics_dict=sub_dict['metrics_dict'])
+            self._ride_list.append(temp_ride_object)
 
     def __str__(self):
         return f"RideHub(n_rides={len(self._ride_list)})"
@@ -42,13 +50,27 @@ class RideHub:
         self._ride_list.append(ride_obj)
 
     def remove_ride(self, ride_id_to_remove):
-
+        """Removes a ride from the internal ride list based on ID"""
         id_set = set([ride.id for ride in self._ride_list])
 
         if ride_id_to_remove not in id_set:
             raise ValueError(f"{ride_id_to_remove} is not contained in this RideHub")
 
         self._ride_list = [ride for ride in self._ride_list if ride_id_to_remove.id != ride_id_to_remove]
+
+    def create_json_output(self):
+
+        """
+        Converts a series of StravaRide objects to one list of dictionaries for writing JSON
+        files
+        """
+
+        return [ride.to_dict() for ride in self._ride_list]
+
+    def get(self, ride_id: int) -> StravaRide:
+        if ride_id not in self.ride_ids:
+            raise ValueError(f"{ride_id} does not exist")
+        return [i for i in self._ride_list if i.id == ride_id][0]
 
     @property
     def ride_list(self):
