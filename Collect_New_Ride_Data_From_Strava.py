d788e01883b3fac3fc16ce5fca2880bfaa457445
fmathews11 <frank.mathews@handleglobal.com>
2024-09-03 10:13:42 -0400

    Split out StravaRide into base module

--- Collect_New_Ride_Data_From_Strava.py
+++ Collect_New_Ride_Data_From_Strava.py
@@ -5,7 +5,8 @@ import time
 import numpy as np
 from modules.api_functions import generate_access_token, get_activity_data
 from modules.create_logger import create_logger
-from modules.objects.RideHub import StravaRide, RideHub
+from modules.objects.RideHub import RideHub
+from modules.objects.Base import StravaRide
 
 logger = create_logger('RideStatusLogger', 'debug')
 endpoint_suffix = ("/streams?keys=time,distance,latlng,altitude,velocity_smooth,heartrate,cadence,watts,temp,moving,"
